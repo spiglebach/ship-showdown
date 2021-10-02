9c7c64d41d35f809ffd03c3010de8890aec5b352
Szukács Marcell <spiglebach@gmail.com>
2021-10-02 12:33:39 +0200

    add ship ramming

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,10 +1,19 @@
+using System;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
     [SerializeField] private bool wasdPlayer;
     [SerializeField] private int health = 100;
+    [SerializeField] private int ramDamage = 45;
+    [SerializeField] private float ramCooldown = 3f;
 
     public bool WasdPlayer => wasdPlayer;
+    
+    private float remainingRamCooldown;
+
+    private void Update() {
+        remainingRamCooldown -= Time.deltaTime;
+    }
 
     public void TakeDamage(int amount) {
         health -= amount;
@@ -17,5 +26,11 @@ public class Player : MonoBehaviour {
         gameObject.SetActive(false);
         // TODO play VFX and SFX
         ScoreSystem.Instance.PlayerDestroyed(this);
+    }
+
+    private void OnCollisionEnter(Collision other) {
+        if (remainingRamCooldown > 0 || !other.gameObject.TryGetComponent(out Player otherPlayer)) return;
+        otherPlayer.TakeDamage(ramDamage);
+        remainingRamCooldown = ramCooldown;
     }
 }
