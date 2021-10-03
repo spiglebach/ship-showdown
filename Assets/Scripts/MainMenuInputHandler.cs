944aa45267a1fadcb3a1923c1ad8889a929d9d2e
Szukács Marcell <spiglebach@gmail.com>
2021-10-03 20:45:07 +0200

    add controls info

--- Assets/Scripts/MainMenuInputHandler.cs
+++ Assets/Scripts/MainMenuInputHandler.cs
@@ -1,12 +1,33 @@
+using System;
 using UnityEngine;
 
 public class MainMenuInputHandler : MonoBehaviour {
+    [SerializeField] private GameObject controlsOverlay;
+
+    private void Start() {
+        CloseControls();
+    }
+
     void Update() {
         if (Input.GetKeyDown(KeyCode.P)) {
             FindObjectOfType<LevelLoader>().Play();
         }
+        if (Input.GetKeyDown(KeyCode.C)) {
+            ShowControls();
+        }
         if (Input.GetKeyDown(KeyCode.Q)) {
             FindObjectOfType<LevelLoader>().Quit();
         }
+        if (Input.GetKeyDown(KeyCode.B)) {
+            CloseControls();
+        }
+    }
+
+    public void ShowControls() {
+        controlsOverlay.SetActive(true);
+    }
+
+    public void CloseControls() {
+        controlsOverlay.SetActive(false);
     }
 }
